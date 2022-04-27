9ffedec7ac65f46da7be6cb4ab7884c605a19dcc
Kaede Oishi <maple_1031@keio.jp>
2022-04-27 10:55:15 +0900

    npm build追加

---
+++ src/js/content_script.js
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+document.onload = () => console.log("init!");
---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+var debug = process.env.NODE_ENV !== "production";
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+  context: path.join(__dirname, "src"),
+  entry: "./js/content_script.js",
+  module: {
+    rules: [{
+      test: /\.jsx?$/,
+      exclude: /(node_modules|bower_components)/,
+      use: [{
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-react', '@babel/preset-env']
+        }
+      }]
+    },
+    {
+      test: /\.css/,
+      use: [
+        "style-loader",
+        {
+          loader: "css-loader",
+          options: { url: false }
+        }
+      ]
+    }]
+  },
+  output: {
+    path: __dirname + "/src/",
+    filename: "content_script.bundle.js"
+  },
+  plugins: debug ? [] : [
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
+  ],
+  devServer: {
+    historyApiFallback: true,
+    static: {
+      directory: path.join(__dirname, "src"),
+    }
+  }
+};
